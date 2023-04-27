1b88d4ba4b65cfb6954de4b9420afecc86b2b719
AnBarr45 <bpena2063@e-camp.cl>
2023-04-27 12:57:05 -0400

    first commit

---
+++ m3-Python_Basico/trabajo1.py
@@ -0,0 +1,8 @@
+def main():#creación de la función main
+   a = 20 #declaración de la variable "a" con valor 20
+   b = 30 #declaración de la variable "b" con valor 30
+   
+   print(a*b)#multiplicamos a por b, e imprimimos el resultado 
+
+
+main()#llamamos a main para ejecutar nuestro código
