96f61cb42ddc14e85a71d8e84d71d49deb24fcfa
AnBarr45 <bpena2063@e-camp.cl>
2023-04-28 12:18:55 -0400

    adicion trabajo2 sesion4

---
+++ m3-Python_Basico/sesion4_trabajo2.py
@@ -0,0 +1,7 @@
+
+persona1 = {"nombre":"josé","apellido": "hernandez","telefono":"+56912345678"}
+persona2 = {"nombre":"mónica","apellido": "suarez","telefono":"+56912398744"}
+persona3 = {"nombre":"ernesto","apellido": "hamilton","telefono":"+56998765432"}
+mi_lista = [persona1,persona2,persona3]
+
+print(mi_lista)
