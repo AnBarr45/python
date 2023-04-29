e5a5a67a36df3c662e8433f6543949425c09b290
AnBarr45 <bpena2063@e-camp.cl>
2023-04-28 21:45:50 -0400

    adicion trabajo 1 sesion 6

---
+++ m3-Python_Basico/sesion6_trabajo1.py
@@ -0,0 +1,19 @@
+def positivo_o_negativo(n):
+    if n < 0:
+        return "negativo"
+    return "positivo"
+
+def num_check(n: int) -> int:
+    r = positivo_o_negativo(n)
+    if n == 0:
+        return "El número es cero"
+    if n % 2 == 0:
+         return f"El número es par y {r} "
+    return f"El número es impar y {r}"
+     
+
+def main():
+    n = int(input("Elija un número: "))
+    print(num_check(n))
+
+main()
