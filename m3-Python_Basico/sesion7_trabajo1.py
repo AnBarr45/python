75c4e4c8bb30755c7fabbe953b2a51aa5fe26ce5
AnBarr45 <bpena2063@e-camp.cl>
2023-04-28 22:20:03 -0400

    adicion trabajo 1 sesion 7

---
+++ m3-Python_Basico/sesion7_trabajo1.py
@@ -0,0 +1,11 @@
+def main():
+    números = [1,2,3,4,5,6,7,8,9,10]
+
+    for i in números:
+        if i % 2 == 0:
+            print(f"{i}: par")
+        else:
+            print(f"{i}: impar")
+
+
+main()
