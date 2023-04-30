537555db7a83be17141379ccfb40f78adb7a1d9a
AnBarr45 <bpena2063@e-camp.cl>
2023-04-29 20:13:06 -0400

    Practica de consolidación módulo 3 terminada

---
+++ m3-Python_Basico/PRACTICA_DE_CONSOLIDACION_3.py
@@ -0,0 +1,27 @@
+def hacer_grandioso(lista):  
+    for idx,nombre in enumerate(lista):
+        lista[idx] = f"El gran {nombre}"
+    
+
+def imprimir_nombres(grupo, lista_nombres):
+    print(f"\n{grupo}:")
+    for nombre in lista_nombres:    
+        print("-",nombre)
+    
+
+
+
+def main():
+    l = ["Harry Houdini","Newton","David Blaine","Hawking","Messi","Teller","Einstein","Pele","Juanes"]
+    magos,cientificos,otros = [[l[0],l[2],l[5]],[l[1],l[3],l[-3]] ,[l[4],l[-2],l[-1]]]
+ 
+    hacer_grandioso(magos)
+
+    imprimir_nombres("Lista original",l)
+    imprimir_nombres("Magos",magos)
+    imprimir_nombres("Científicos",cientificos)
+    imprimir_nombres("Restantes",otros)
+    
+
+
+main()
