5c0562dc760ce2430cde859436e845e775da0e28
AnBarr45 <bpena2063@e-camp.cl>
2023-05-08 15:48:50 -0400

    pt 1 listo

--- m4_python_avanzado/practica de consolidacion/Practica_de_consolidacion_m4.py
+++ m4_python_avanzado/practica de consolidacion/Practica_de_consolidacion_m4.py
@@ -10,5 +10,31 @@ class Automovil(Vehiculo):
         self.velocidad = velocidad
         self.cilindrada = cilindrada
         super().__init__(marca, modelo, numero_ruedas)
+        
+    def __str__(self) -> str:
+        tmp = super().__str__()
+        return f"Datos del automovil => Marca: {self.marca}, Modelo: {self.modelo}, {self.numero_ruedas} Ruedas,{self.velocidad} km/hr, {self.cilindrada} cc"
+
 
     
+def obtener_datos_vehiculo(n_vehiculos: int):
+    v_array = []
+    for _ in range(0,n_vehiculos):
+        marca = input("Inserte la marca del automovil: ")
+        modelo = input("Inserte el modelo del automovil: ")
+        n_ruedas = input("Inserte el numero de ruedas del automovil: ")
+        velocidad = int(input("Inserte la velocidad del automovil en km/hr: "))
+        cilindraje = int(input("Inserte el cilindraje del automovil: "))
+        
+        instancia = Automovil(marca,modelo,n_ruedas,velocidad,cilindraje)
+        v_array.append(instancia) 
+    
+    return v_array
+
+
+def main():
+    n_vehiculos = int(input("Cuantos Vehiculos desea insertar: "))
+    vehiculo_instancias = obtener_datos_vehiculo(n_vehiculos)
+    for i in vehiculo_instancias:
+        print(i)
+main()
