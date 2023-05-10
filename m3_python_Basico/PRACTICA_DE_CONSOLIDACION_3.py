5872071f233a45063f822157874f36b5e4792eb2
AnBarr45 <bpena2063@e-camp.cl>
2023-05-10 11:56:11 -0400

    dejar los grupos en líneas distintas

--- m3_python_Basico/PRACTICA_DE_CONSOLIDACION_3.py
+++ m3_python_Basico/PRACTICA_DE_CONSOLIDACION_3.py
@@ -7,14 +7,13 @@ def imprimir_nombres(grupo, lista_nombres):
     print(f"\n{grupo}:")
     for nombre in lista_nombres:    
         print("-",nombre)
-    
-
-
 
 def main():
     l = ["Harry Houdini","Newton","David Blaine","Hawking","Messi","Teller","Einstein","Pele","Juanes"]
-    magos,cientificos,otros = [[l[0],l[2],l[5]],[l[1],l[3],l[-3]] ,[l[4],l[-2],l[-1]]]
- 
+    magos = [l[0],l[2],l[5]]
+    cientificos = [l[1],l[3],l[-3]] 
+    otros = [l[4],l[-2],l[-1]]
+
     hacer_grandioso(magos)
 
     imprimir_nombres("Lista original",l)
@@ -22,6 +21,4 @@ def main():
     imprimir_nombres("Científicos",cientificos)
     imprimir_nombres("Restantes",otros)
     
-
-
 main()
