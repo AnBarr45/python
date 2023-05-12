5790ecedf0a532910ad79c9a71774e5633c79a2c
AnBarr45 <bpena2063@e-camp.cl>
2023-05-12 16:24:53 -0400

    adicion metodo __str__ a las clases

--- m4_python_avanzado/practica de consolidacion/vehiculo.py
+++ m4_python_avanzado/practica de consolidacion/vehiculo.py
@@ -17,31 +17,46 @@ class Automovil(Vehiculo):
         
         
     def __str__(self) -> str:
-        return super().__str__() + f"{self.velocidad} km/hr | {self.cilindrada} cc|"
+        return super().__str__() + f" {self.velocidad} km/hr | {self.cilindrada} cc|"
         
 # ################################## PARTE 2 #################################### #
 class Particular(Automovil):
     def __init__(self,num_puestos,*args,**kwargs) -> None:
         self.num_puestos = num_puestos
         super(Particular,self).__init__(*args,**kwargs)
+        
+    def __str__(self) -> str:
+        return super().__str__() + f" {self.num_puestos} |"
 
 class Carga(Automovil):
     def __init__(self,peso_carga,*args,**kwargs) -> None:
         self.peso_carga = peso_carga
         super(Carga,self).__init__(*args,**kwargs)
 
+    def __str__(self) -> str:
+        return super().__str__() + f" {self.peso_carga} |"
+    
 class Bicicleta(Vehiculo):   
     def __init__(self,*args,**kwargs) -> None:
         super(Bicicleta,self).__init__(*args,**kwargs)
         
+    def __str__(self) -> str:
+        return super().__str__()
+    
 class Urbana(Bicicleta):
     def __init__(self,*args,**kwargs) -> None:
         super(Urbana,self).__init__(*args,**kwargs)
-
+    def __str__(self) -> str:
+        return super().__str__()
+    
 class Carrera(Bicicleta):
     def __init__(self,*args,**kwargs) -> None:
         super(Carrera,self).__init__(*args,**kwargs)
-
+    def __str__(self) -> str:
+        return super().__str__()
+    
 class Motocicleta(Bicicleta):
     def __init__(self,*args,**kwargs) -> None:
         super(Motocicleta,self).__init__(*args,**kwargs)
+    def __str__(self) -> str:
+        return super().__str__()
