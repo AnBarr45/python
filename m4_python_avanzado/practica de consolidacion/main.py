99c234f48b0c7dd18e48d65b9c8e68f890f11747
AnBarr45 <bpena2063@e-camp.cl>
2023-05-15 18:40:10 -0400

    adicion funcion para imprimir contenidos de pt1

--- m4_python_avanzado/practica de consolidacion/main.py
+++ m4_python_avanzado/practica de consolidacion/main.py
@@ -1,6 +1,21 @@
 from vehiculo import *
-
+def pt1():
+    lista_autos = [] 
+    for i in range(1,4):
+        print(f"Ingrese los datos del vehiculo {i} => ")
+        marca = input("Inserte la marca del vehiculo: ")
+        modelo = input("Inserte la modelo del vehiculo: ")
+        nro_ruedas = int(input("Inserte el numero de ruedas del vehiculo: "))
+        velocidad = int(input("Inserte la velocidad del vehiculo: "))
+        cilindrada = int(input("Inserte la cilindrada del vehiculo: "))
+        obj = Automovil(marca,modelo,nro_ruedas,velocidad=velocidad,cilindrada=cilindrada)
+        lista_autos.append(obj)
+        
+    for i in range(0,3):
+        print(f"Datos del vehiculo {i + 1}")
+        print(lista_autos[i])
 def main():
+    pt1()
     vehiculo_base = Vehiculo("marca","modelo",4) 
     auto_base = Automovil("marca","modelo",4,velocidad=100,cilindrada=800)
     auto_particular = Particular("Ford","Fiesta",4,velocidad=180,cilindrada=500,num_puestos=5)
