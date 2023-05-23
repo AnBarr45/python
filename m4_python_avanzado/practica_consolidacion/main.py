f351b58b99f574dd600ce23cf2b24271bc8b2cb4
AnBarr45 <bpena2063@e-camp.cl>
2023-05-23 19:54:43 -0400

    adicion comentario explicativo

--- m4_python_avanzado/practica_consolidacion/main.py
+++ m4_python_avanzado/practica_consolidacion/main.py
@@ -2,7 +2,7 @@ from pt1 import *
 from pt2 import *
 from pt3 import *
 
-def main():
+def main(): # lo dividi en partes para poder comentarlas facilmente y asi aislarlas.
     parte1()
     parte2()
     parte3()   
