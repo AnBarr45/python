109fa92a7358bdc41f80386fb93be003cc991a41
AnBarr45 <bpena2063@e-camp.cl>
2023-05-23 15:55:02 -0400

    try/ except basico añadido

--- m4_python_avanzado/practica de consolidacion/vehiculo.py
+++ m4_python_avanzado/practica de consolidacion/vehiculo.py
@@ -8,20 +8,38 @@ class Vehiculo:
     def __str__(self) -> str:
         return f"| Marca: {self.marca} | Modelo: {self.modelo} | {self.num_ruedas} Ruedas |"
     
-    def guardar_datos_csv(self,archivo_destino):
-        archivo = open(archivo_destino, "w")
-        datos = [(self.__class__, self.__dict__)]
-        archivo_csv = csv.writer(archivo)
-        archivo_csv.writerows(datos)
-        archivo.close()
+    def guardar_datos_csv(self,archivo):
+        try:
+            archivo = open(archivo, "w")   
+        except IsADirectoryError:
+            print(f"'{archivo}' is a Directory, not a file")
+        except PermissionError:
+            exit(f"No permission to write to file {archivo}")
+        except OSError:
+            exit()
+        else:
+            datos = [(self.__class__, self.__dict__)]
+            archivo_csv = csv.writer(archivo)
+            archivo_csv.writerows(datos)
+            archivo.close()
+            
     def leer_datos_csv(self,archivo):
-        vehiculos = []
-        archivo = open(archivo, "r")
-        archivo_csv = csv.reader(archivo)
-        for vehiculo in archivo_csv:
-            vehiculos.append(vehiculo)
-        archivo.close()
-        return vehiculos
+        try:
+            archivo = open(archivo, "r")   
+        except PermissionError:
+            exit(f"No permission to read the contents of file {archivo}")
+        except IsADirectoryError:
+            print(f"'{archivo}' is a Directory, not a file")
+        except FileNotFoundError:
+            print(f"File {archivo} does not exist or can't be found")
+        else:
+            vehiculos = []
+            archivo_csv = csv.reader(archivo)
+            for vehiculo in archivo_csv:
+                vehiculos.append(vehiculo)
+            archivo.close()
+            return vehiculos
+        
 ##########################################################
 class Automovil(Vehiculo):
     def __init__(self,*args, velocidad: int, cilindrada: int) -> None:
