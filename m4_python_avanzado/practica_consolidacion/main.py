7f613087c647c324af1b736e51d64eeed3115e1f
AnBarr45 <bpena2063@e-camp.cl>
2023-05-26 09:40:40 -0400

    arreglo, menu sencillo

--- m4_python_avanzado/practica_consolidacion/main.py
+++ m4_python_avanzado/practica_consolidacion/main.py
@@ -2,8 +2,14 @@ from pt1 import *
 from pt2 import *
 from pt3 import *
 
-def main(): # lo dividi en partes para poder comentarlas facilmente y asi aislarlas.
-    parte1()
-    parte2()
-    parte3()   
+def main():
+    print("Que parte desea ejecutar? (pt1, pt2 o pt3):")
+    pt = int(input("numero:"))
+    
+    
+    if pt == 1: parte1()
+    elif pt == 2: parte2()
+    elif pt == 3: parte3()
+    else:
+        exit("el numero debe ser 1, 2 o 3")
 main()
